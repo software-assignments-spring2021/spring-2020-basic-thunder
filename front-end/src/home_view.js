c3a69340ac1aa04ad57413f0bdb05fef7eacc3a5
sylviaji47 <yiminji98@gmail.com>
2020-03-22 19:20:24 -0400

    update branch

--- front-end/src/home_view.js
+++ front-end/src/home_view.js
@@ -4,27 +4,45 @@ import {Link} from "react-router-dom"
 
 const HomeView = function() {
     return (
-        <div>
-            <h1>Welcome to Biazza!</h1>
-            <p>Forums, schedules, and syllabuses for college courses</p>
-            <LoginBtn/>
-            <RegisterBtn/>
+        <div class="container">
 
-            <ul>
-                <li>Create and join classes</li>
-                <li>Upload and access schedules and syllabuses</li>
-                <li>Post questions for instructors and classmates</li>
-                <li>Reply to classmates' posts</li>
-                <li>Rank answers based on helpfulness</li>
-            </ul>
+            <div id="header">
+                <Header/>
+            </div>
 
-            <div>
-                <img src="../images/placeholder.jpg" alt="placeholder"/>
+
+            <div class="main">
+                <h1>Welcome to Biazza!</h1>
+                <p>Forums, schedules, and syllabuses for college courses</p>
+                <LoginBtn/>
+                <RegisterBtn/>
+
+                <ul>
+                    <li>Create and join classes</li>
+                    <li>Upload and access schedules and syllabuses</li>
+                    <li>Post questions for instructors and classmates</li>
+                    <li>Reply to classmates' posts</li>
+                    <li>Rank answers based on helpfulness</li>
+                </ul>
+
+                <div>
+                    <img src="../images/placeholder.jpg" alt="placeholder"/>
+                </div>
+
             </div>
 
         </div>
     )
+}
 
+const Header = function() {
+    return (
+        <Link to='/home'>
+            <a>
+                Biazza
+            </a>
+        </Link>
+    )
 }
 
 const LoginBtn = function() {
@@ -46,6 +64,5 @@ const RegisterBtn = function() {
         </Link>
     )
 }
-
 
 export {HomeView}
