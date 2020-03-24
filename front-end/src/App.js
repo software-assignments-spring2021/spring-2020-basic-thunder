87bccf70fdfb7719e599150c1c198e28432aa1ba
sylviaji47 <yiminji98@gmail.com>
2020-03-24 19:06:08 -0400

    Revert "Merge pull request #58 from nyu-software-engineering/user-story/41/task-21/create-home-view-not-logged-in"
    This reverts commit f9a1a180ed3eb3ef753d051fc15f5fd576c4d381, reversing
    changes made to 7fc601a75c019eb96f7016e8165ce883bd0dc418.
    
    revert merge

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,10 +7,6 @@ import {HomeView} from "./logged_in_home_view";
 import {ListPostsView} from "./list_posts_view"
 import {PostView} from "./view_post_view"
 import {ReplyPostView} from "./reply_post_view"
-import {NotLoggedInHomeView} from './home_view'
-import {LoginView} from './login_view'
-import {RegisterView} from './register_view'
-
 import {
     BrowserRouter as Router,
     Switch,
@@ -24,9 +20,6 @@ function App() {
         <Router>
             <div className="App">
                 <Switch>
-                    <Route path={"/home"} component={NotLoggedInHomeView} />
-                    <Route path="/sign-up" component={RegisterView} />
-                    <Route path="/log-in" component={LoginView} />
                     <Route path="/LoggedInHome/" component={HomeView} />
                     <Route path="/:courseId/Forum/:postId/post/ReplyPost" component={ReplyPostView}/>
                     <Route path="/:courseId/Forum/:postId/post" component={PostView}/>
